87ffa70ab1356aaf95bdac6c7e1f5a01a67ec922
c-wilton <26773381+c-wilton@users.noreply.github.com>
2022-07-27 23:46:52 +0100

    Update test2.js

--- seawatcher/test2.js
+++ seawatcher/test2.js
@@ -1,31 +1,28 @@
 function geoFindMe() {
     
-    const atext = document.querySelector('a-text');
+    const atext = document.querySelector("a-text");
     
     function success(position) {
         console.log("Location Success")
         const latitude  = position.coords.latitude;
         const longitude = position.coords.longitude;
         
-        atext.setAttribute(value, '');
+        atext.setAttribute(value,"");
         atext.setAttribute(value, "Latitude: ${latitude}, Longitude: ${longitude}");
         atext.setAttribute("gps-entity-place", "latitude: ${latitude}; longitude: ${latitude}");
     }
       
     function error() {
-        atext.value = 'Unable to retrieve your location';
+        atext.value = "Unable to retrieve your location";
     }
       
     if(!navigator.geolocation) {
-        atext.value = 'Geolocation is not supported by your browser';
+        atext.value = "Geolocation is not supported by your browser";
     } else {
-        atext.value = 'Locating…';
+        atext.value = "Locating…";
         navigator.geolocation.getCurrentPosition(success, error);
     }
     
 }
 
-/*
-document.querySelector('#find-me').addEventListener('click', geoFindMe);
-*/
 window.onload = () => { geoFindMe() };
