d05acedc0d723c1fb84def72caf70736b11d6025
c-wilton <26773381+c-wilton@users.noreply.github.com>
2022-08-12 20:06:32 +0100

    Update RulerContoller.js

--- seawatcher/RulerContoller.js
+++ seawatcher/RulerContoller.js
@@ -1,6 +1,6 @@
 function createRuler() {
   const camera = document.getElementById("camera");
-  let el = document.createElement(a-entity);
+  let el = document.createElement("a-entity");
   camera.append(el)
 }
 
