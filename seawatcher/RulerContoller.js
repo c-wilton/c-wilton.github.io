8862d47c65cff8d72f8653d1f74de396341af2d5
c-wilton <26773381+c-wilton@users.noreply.github.com>
2022-08-14 23:58:10 +0100

    Update RulerContoller.js

--- seawatcher/RulerContoller.js
+++ seawatcher/RulerContoller.js
@@ -19,7 +19,7 @@ class Controller {
     aEntity = this.view.createElement("a-entity")
     aEntityAttr = {}
     //add for loop
-    let line = this.setLine(-1.3, -1.7, -3, -1, 1.7, -3, red)
+    let line = this.setLine(-1.3, -1.7, -3, -1, 1.7, -3, 'red')
     aEntityAttr['line'] = line
     
     //add all lines to aEntity
