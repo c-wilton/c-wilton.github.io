95dcbc0a201f0a22a4a9c6866d53fbb98cc49f22
c-wilton <26773381+c-wilton@users.noreply.github.com>
2022-08-18 20:58:55 +0100

    Update device.js

--- seawatcher/device.js
+++ seawatcher/device.js
@@ -4,4 +4,4 @@ let usableHeight = window.screen.height
 console.log('device height: ', deviceHeight)
 console.log('in cm: ', deviceHeight/3)
 console.log('usable height: ', usableHeight)
-consol.log('in cm: ' , usableHeight/3)
+console.log('in cm: ' , usableHeight/3)
