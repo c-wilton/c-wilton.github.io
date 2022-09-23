0bd3e12c546f08b2252d135cefe7cebf7a99c3cc
c-wilton <26773381+c-wilton@users.noreply.github.com>
2022-09-24 00:02:41 +0100

    Update LocationModel.js
    add DocString

--- seawatcher/LocationModel.js
+++ seawatcher/LocationModel.js
@@ -1,4 +1,5 @@
 class LocationModel {
+  /** Gets the location of the device */
   
   #latitude
   #longitude
@@ -6,12 +7,14 @@ class LocationModel {
   #altitude
   
   constructor() {
+    /** sets the location to default values, then attempts to retrieve device location */
     console.log('New Location model')
     this.setLocation()
     this.getGeoLocation()
   }
   
   setLocation(){
+    /** Sets the location to default values */
     let defaultLatitude = 53.225589
     let defaultLongitude = -4.127876
     let defaultAltitude = 0
@@ -25,29 +28,41 @@ class LocationModel {
   }
   
   getLocation(){
+    /** @return the location */
     console.log('getLocation')
     return this.#location
   }
   
   success(position) {
+    /** if geolocation was a success, get the device location
+        * @param position the device location
+    */
+    
+    /* get device location */
     console.log("Location Success")
     const latitude  = position.coords.latitude;
     const longitude = position.coords.longitude;
     const altitude = position.coords.altitude;
+    
+    /* print the location to the console */
     let place = 'latitude: ' + latitude + ': longitude: ' + longitude
     console.log('place: ', place)
     console.log('altitude', altitude)
   }
   
   error() {
+    /** display message if geolocation failed */
     console.log("Location error")
   }
   
   getGeoLocation() {
+    /** Attempt to get the device location */
     console.log('getGeoLocation')
     if(!navigator.geolocation) {
+      /** display message if geolocationis not supported */
       console.log("Location not supported")
     } else {
+      /** if geolocation is supported, get the device location */
       navigator.geolocation.getCurrentPosition(this.success, this.error);
     } 
   }
