c68920fc4fe837eb4e12231f41af5dfb94f3de0d
c-wilton <26773381+c-wilton@users.noreply.github.com>
2023-01-01 20:27:40 +0000

    Update RulerModel.js

--- seawatcher/RulerModel.js
+++ seawatcher/RulerModel.js
@@ -70,8 +70,8 @@ class RulerModel {
   
   setDistances() {
     /** Sets the distances to pre-defined values */
-    this.#distances = [100, 200, 300, 500, 1000, 5000, 10000]
-    //this.#distances = [100, 200, 300, 500, 700, 1000, 1500, 2000, 3000, 5000, 10000]
+    //this.#distances = [100, 200, 300, 500, 1000, 5000, 10000]
+    this.#distances = [100, 200, 300, 500, 700, 1000, 1500, 2000, 3000, 5000, 10000]
   }
   
   setIntervals() {
